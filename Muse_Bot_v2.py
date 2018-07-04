0f867d9607a7306987764304013dc125031c927b
cdpham42 <cdpham42@gmail.com>
2018-07-03 21:25:01 -0700

    Create basic bot

--- Muse_Bot_v2.py
+++ Muse_Bot_v2.py
@@ -21,7 +21,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    if message.content.startswith('!muse hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
